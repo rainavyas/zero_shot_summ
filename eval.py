28b0cf3490021e0b46f7a4ff44c625554008e1e0
Vyas Raina <sayvaniar@gmail.com>
2023-03-21 23:49:08 +0000

    evaluate multiple output folds in one go

--- eval.py
+++ eval.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     # Get command line arguments
     commandLineParser = argparse.ArgumentParser()
-    commandLineParser.add_argument('--filepath', type=str, required=True, help='path to data with predicted summaries')
+    commandLineParser.add_argument('--filepaths', type=str, nargs='+', required=True, help='path to data outputs with predicted summaries')
     commandLineParser.add_argument('--pred_name', type=str, default='ChatGPT Summary', help='column name for predicted summaries')
     commandLineParser.add_argument('--rouge', action='store_true', help='eval rouge score')
     args = commandLineParser.parse_args()
@@ -23,7 +23,10 @@ if __name__ == "__main__":
         f.write(' '.join(sys.argv)+'\n')
 
     # Load data
-    data = pd.read_csv(args.filepath)
+    datas = []
+    for filepath in args.filepaths:
+        datas.append(pd.read_csv(filepath))
+    data = pd.concat(datas)
     
     if args.rouge:
         rouge1 = []
