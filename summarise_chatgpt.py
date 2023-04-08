cdbabc398cfc15fa54e009a3a925996a49d77176
Vyas Raina <sayvaniar@gmail.com>
2023-04-09 00:13:05 +0100

    ensemble

--- summarise_chatgpt.py
+++ summarise_chatgpt.py
@@ -8,6 +8,7 @@ import os
 import argparse
 import pandas as pd
 from tqdm import tqdm
+from collections import defaultdict
 
 from src.tools.tools import content_merge
         
@@ -22,6 +23,7 @@ if __name__ == "__main__":
     commandLineParser.add_argument('--sys_prompt_ind', type=int, default=0, help='file for system prompts')
     commandLineParser.add_argument('--user_prompt_path', type=str, default='src/prompts/user_prompts.txt', help='file for user prompts')
     commandLineParser.add_argument('--user_prompt_ind', type=int, default=0, help='select user prompt')
+    commandLineParser.add_argument('--num_seeds', type=int, default=10, help='Generate an ensemble of outputs')
     args = commandLineParser.parse_args()
 
     # Save the command run
@@ -42,18 +44,19 @@ if __name__ == "__main__":
         user_prompts = f.readlines()
     user_prompt = user_prompts[args.user_prompt_ind].rstrip('\n') 
 
-    gpt_summs = []
+    gpt_summs = defaultdict(list)
     for _, row in tqdm(data.iterrows(), total=len(data)):
         content = content_merge(row)
         msgs = [
             {"role": "system", "content": sys_prompt},
             {"role": "user", "content": f'{user_prompt}\n{content}'}
         ]
-        response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=msgs)
-        gpt_summs.append(response['choices'][0]['message']['content'])
+        for seed in range(args.num_seeds):
+            response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=msgs)
+            gpt_summs[f'gpt3.5_summary_{seed}'].append(response['choices'][0]['message']['content'])
         # time.sleep(3.1) # necessary for open ai rate limit
     
-    data['ChatGPT Summary'] = gpt_summs
+    data = pd.concat([data, pd.DataFrame(gpt_summs)], axis=1)
 
     # save data
     out_file = f'{args.out_dir}/chatgpt_system{args.sys_prompt_ind}_user{args.user_prompt_ind}.csv'
